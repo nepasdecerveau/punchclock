0ee7d36a8157036a407a9cb740f30417efcf61f4
Ashe Kami <anstasia.kamyshanskaya@lernende.bbw.ch>
2021-09-06 13:14:15 +0200

    User login problem corrected find in PunchclockApplication

--- src/main/java/ch/zli/m223/punchclock/PunchclockApplication.java
+++ src/main/java/ch/zli/m223/punchclock/PunchclockApplication.java
@@ -1,18 +1,27 @@
 package ch.zli.m223.punchclock;
 
+import ch.zli.m223.punchclock.domain.ApplicationUser;
+import ch.zli.m223.punchclock.service.ApplicationUserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class PunchclockApplication {
+public class PunchclockApplication implements CommandLineRunner {
+
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final ApplicationUserService applicationUserService;
+
+	public PunchclockApplication(ApplicationUserService applicationUserService) {
+		this.applicationUserService = applicationUserService;
+	}
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
+		bCryptPasswordEncoder =  new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
 	}
 
 	public static void main(String[] args) {
@@ -20,4 +29,11 @@ public class PunchclockApplication {
 	}
 
 
+	@Override
+	public void run(String... args) throws Exception {
+		ApplicationUser applicationUser = new ApplicationUser();
+		applicationUser.setUsername("admin");
+		applicationUser.setPassword(bCryptPasswordEncoder.encode("admin"));
+		applicationUserService.tryAddUser(applicationUser);
+	}
 }
